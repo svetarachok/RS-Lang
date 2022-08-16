4d487b9428dc9366694f4c79a75c9d194a55e286
SiarheiHa <belposchta@gmail.com>
2022-08-17 02:22:22 +0300

    feat: add createUser and authorize methods to Api

--- src/components/api.ts
+++ src/components/api.ts
@@ -2,6 +2,15 @@ const BASE_LINK = 'http://localhost:8088';
 
 enum Endpoint {
   words = '/words',
+  users = '/users',
+  signin = '/signin',
+}
+enum ContentType {
+  json = 'application/json',
+}
+
+enum HTTPMethod {
+  POST = 'POST',
 }
 
 interface Word {
@@ -21,6 +30,25 @@ interface Word {
   textExampleTranslate: string,
 }
 
+interface UserData {
+  name: string
+  email: string
+  password: string
+}
+
+interface AuthorizationData {
+  message: string,
+  token: string,
+  refreshToken: string,
+  userId: string,
+  name: string,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface NewUser extends Pick<UserData, 'name' | 'email'> {
+  id: string,
+}
+
 const generateQueryString = (queryParam: Record<string, string>): string => `?${Object.keys(queryParam)
   .map((key: string) => `${key}=${queryParam[key]}`)
   .join('&')}`;
@@ -35,15 +63,45 @@ const makeUrl = (
 };
 
 export default class Api {
-  async getWords(queryParam?: { group: string, page: string }): Promise<Word[]> {
+  public async getWords(queryParam?: { group: string, page: string }): Promise<Word[]> {
     const url: URL = makeUrl(BASE_LINK, Endpoint.words, queryParam);
     const response = await fetch(url);
     return response.json();
   }
 
-  async getWordById(id: string): Promise<Word> {
+  public async getWordById(id: string): Promise<Word> {
     const url: URL = makeUrl(BASE_LINK, Endpoint.words);
     const response = await fetch(`${url}/${id}`);
+    return response.json();
+  }
+
+  public async createUser(userData: UserData): Promise<NewUser | string> {
+    const response = await fetch(makeUrl(BASE_LINK, Endpoint.users), {
+      method: HTTPMethod.POST,
+      headers: {
+        'Content-Type': ContentType.json,
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (response.status === 417) {
+      return response.text();
+    }
+    return response.json();
+  }
+
+  public async authorize(authData: Pick<UserData, 'email' | 'password'>): Promise<AuthorizationData | string> {
+    const response = await fetch(makeUrl(BASE_LINK, Endpoint.signin), {
+      method: HTTPMethod.POST,
+      headers: {
+        'Content-Type': ContentType.json,
+      },
+      body: JSON.stringify(authData),
+    });
+
+    if (response.status === 403) {
+      return 'Incorrect e-mail or password';
+    }
     return response.json();
   }
 }
