d1e6669e78014f1a7172a0e6f02ddc83b6864caf
Sveta Rachok <sveta.r1212@gmail.com>
2022-08-16 11:29:59 +0300

    init: start project, configure webpack, eslint and src structure

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "extends": [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module",
+      "project": './tsconfig.json'
+  },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+    "no-debugger": "off",
+    "no-console": 0,
+    "class-methods-use-this": "off",
+    "@typescript-eslint/no-explicit-any": 2,
+    "@typescript-eslint/no-inferrable-types": 0,
+    "import/prefer-default-export": "off",
+    "linebreak-style": 0,
+}
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+              test: /\.html$/i,
+              loader: "html-loader",
+            },
+            { 
+              test: /\.ts$/i, 
+              use: 'ts-loader' 
+            },
+            {
+              test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+              type: 'asset/resource',
+              generator: {
+                filename: './img/[name][ext][query]',
+              },
+            },
+        ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        new CopyPlugin({
+          patterns: [
+            {
+              from: path.resolve(__dirname, 'src', 'img'),
+              to: path.resolve(__dirname, 'dist', 'img'),
+            }
+          ],
+        }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
