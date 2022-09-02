1a152ebdfdb282c8ffcbce6f67b52b924b10b546
Sveta Rachok <sveta.r1212@gmail.com>
2022-09-02 16:14:23 +0300

    feat: add condition - not to change page style when all words are hard

--- src/components/utils/checkPageAllDone.ts
+++ src/components/utils/checkPageAllDone.ts
@@ -1,15 +1,16 @@
+import { WORDS_PER_PAGE } from './constants';
+
 export function checkPageAllDone() {
-  const cards: HTMLElement[] = [...document.querySelectorAll('.card')] as HTMLElement[];
+  // const cards: HTMLElement[] = [...document.querySelectorAll('.card')] as HTMLElement[];
   const pageWrapper: HTMLElement = document.querySelector('.text-book-page') as HTMLElement;
   const pageNumber: HTMLInputElement = document.querySelector('.page-input') as HTMLInputElement;
   const audioCallBtn = document.querySelector('[href="/book/audiocall"]');
   const sprintBtn = document.querySelector('[href="/book/sprint"]');
-  const res = cards.every((card) => {
-    const hardBtn = card.querySelector('.btn-add');
-    const learnBtn = card.querySelector('.btn-learn');
-    return (hardBtn?.classList.contains('hard-word-btn') || learnBtn?.classList.contains('learn-word-btn'));
-  });
-  if (res) {
+  const hardBtns = document.querySelectorAll('.hard-word-btn') as NodeList;
+  const learnBtns = document.querySelectorAll('.learn-word-btn') as NodeList;
+  const conditionEvery = ((learnBtns.length + hardBtns.length) === 20
+   && learnBtns.length > 0);
+  if ((learnBtns.length === WORDS_PER_PAGE) || conditionEvery) {
     pageWrapper.style.border = '3px solid #332a7c';
     pageNumber.style.border = '3px solid #332a7c';
     if (audioCallBtn) audioCallBtn.classList.add('btn__disabled');
