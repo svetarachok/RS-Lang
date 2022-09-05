f7a16ab483c138be43cdcd5e97353b451cff006a
SiarheiHa <belposchta@gmail.com>
2022-09-05 15:58:51 +0300

    fix: fix location

--- src/components/controller/Controller.ts
+++ src/components/controller/Controller.ts
@@ -99,9 +99,9 @@ export class Controller {
     this.registerForm.listenForm(this.handleRegistartion.bind(this));
     this.userUI.unAuthorize(this.handleUnLogin.bind(this));
     this.handleUser();
-    // if (!window.location.href.match(/#\/$/)) {
-    //   window.location.href = `${window.location.href}#/`;
-    // }
+    if (!window.location.href.match(/#\/$/)) {
+      window.location.href = `${window.location.href}#/`;
+    }
     this.router.updatePageLinks();
   }
 
